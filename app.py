c0ae333ad49571814fc88be08c191979134ab869
NIGEL WONG XIN XIANG <2305166G@student.tp.edu.sg>
2025-10-31 15:52:20 +0800

    Added .README

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv():
     st.set_page_config(page_title="Chat with multiple PDFs")
     st.header("Chat with multiple PDFs")
     st.text_input("Ask a question!")
