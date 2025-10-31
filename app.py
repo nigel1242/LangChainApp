0eeb3392e843168370a8820f41aee7b4bfe1c34e
NIGEL WONG XIN XIANG <2305166G@student.tp.edu.sg>
2025-10-31 15:37:40 +0800

    Added app.py, requirements.txt, and .gitignore

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs")
+    st.header("Chat with multiple PDFs")
+    st.text_input("Ask a question!")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here")
+        st.button("Process")
+
+
+
+if __name__ == '__main__':
+    main()
+
